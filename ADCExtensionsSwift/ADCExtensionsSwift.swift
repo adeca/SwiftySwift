e50a3de193afa64c10d7a6c47f2ec3315d27c36b
Agustín de Cabrera <Agustin.DeCabrera@rga.com>
2015-06-29 18:58:15 -0300

    Fixed compile error with Swift 2.0 beta 2

--- ADCExtensionsSwift/ADCExtensionsSwift.swift
+++ ADCExtensionsSwift/ADCExtensionsSwift.swift
@@ -10,17 +10,21 @@ import Foundation
 
 // MARK: dispatch
 
+/// Submits a block for asynchronous execution on a global queue with the given identifier
 public func dispatch_async(identifier: Int, _ block: dispatch_block_t) {
     dispatch_async(dispatch_get_global_queue(identifier, 0), block)
 }
+/// Submits a block for asynchronous execution on the main queue
 public func dispatch_async(block: dispatch_block_t) {
     dispatch_async(dispatch_get_main_queue(), block)
 }
 
+/// Enqueue a block for execution at the specified time (given in seconds)
 public func dispatch_after(delay: NSTimeInterval, _ queue: dispatch_queue_t, _ block: dispatch_block_t) {
     let time = dispatch_time(DISPATCH_TIME_NOW, Int64(delay * NSTimeInterval(NSEC_PER_SEC)))
     dispatch_after(time, queue, block)
 }
+/// Enqueue a block for execution on the main queue at the specified time (given in seconds)
 public func dispatch_after(delay: NSTimeInterval, _ block: dispatch_block_t) {
     dispatch_after(delay, dispatch_get_main_queue(), block)
 }
@@ -171,7 +175,7 @@ extension UIImage {
     }
     
     public class func tiledImageNamed(name: String) -> UIImage? {
-        return self(named: name)?.resizableImageWithCapInsets(UIEdgeInsetsZero)
+        return self.init(named: name)?.resizableImageWithCapInsets(UIEdgeInsetsZero)
     }
     
     public func imageWithOrientation(orientation: UIImageOrientation) -> UIImage? {
