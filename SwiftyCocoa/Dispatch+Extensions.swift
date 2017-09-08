f166e79faf6f45d0994eb784658c2ed276844f4d
Agustín de Cabrera <agustindc@gmail.com>
2017-09-08 14:50:03 -0300

    Deprecated dispatch methods.

--- SwiftyCocoa/Dispatch+Extensions.swift
+++ SwiftyCocoa/Dispatch+Extensions.swift
@@ -20,19 +20,23 @@ extension DispatchQueue {
 // MARK: - dispatch
 
 /// Submits a block for asynchronous execution on a global queue with the given identifier
+@available(*, deprecated)
 public func async(qos: DispatchQoS.QoSClass, execute block: @escaping () -> Void) {
     DispatchQueue.global(qos: qos).async(execute: block)
 }
 /// Submits a block for asynchronous execution on the main queue
+@available(*, deprecated)
 public func async(execute block: @escaping ()->()) {
     DispatchQueue.main.async(execute: block)
 }
 
 /// Enqueue a block for execution on the main queue at the specified time (given in seconds)
+@available(*, deprecated)
 public func async(afterDelay delay: TimeInterval, qos: DispatchQoS.QoSClass, execute block: @escaping @convention(block) () -> Void) {
     DispatchQueue.global(qos: qos).asyncAfter(delay: delay, execute: block)
 }
 /// Enqueue a block for execution on the main queue at the specified time (given in seconds)
+@available(*, deprecated)
 public func asyncAfter(delay: TimeInterval, execute block: @escaping @convention(block) () -> Void) {
     DispatchQueue.main.asyncAfter(delay: delay, execute: block)
 }
